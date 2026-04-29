64578fc6adcd467744cc4d9e1f38d8d5a5320659
Marsraad_ <86083007+PanzerschokoladeAddict@users.noreply.github.com>
2026-04-29 17:25:51 +1000

    Remove onboard function

--- app/join.tsx
+++ app/join.tsx
@@ -1,25 +1,11 @@
 import * as React from "react";
-import { Image, StyleSheet } from "react-native";
-import Onboarding from "react-native-onboarding-swiper";
+import { StyleSheet } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Join() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <Onboarding
-          pages={[
-            {
-              backgroundColor: "#fff",
-              image: (
-                <Image source={require("../assets/images/react-logo.png")} />
-              ),
-              title: "Join An Existing Team",
-              subtitle: "Enter your team code to join an existing team.",
-            },
-          ]}
-        />
-      </SafeAreaView>
+      <SafeAreaView style={styles.container}></SafeAreaView>
     </SafeAreaProvider>
   );
 }
--- app/setup.tsx
+++ app/setup.tsx
@@ -1,25 +1,11 @@
 import * as React from "react";
-import { Image, StyleSheet } from "react-native";
-import Onboarding from "react-native-onboarding-swiper";
+import { StyleSheet } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function SetUp() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <Onboarding
-          pages={[
-            {
-              backgroundColor: "#fff",
-              image: (
-                <Image source={require("../assets/images/react-logo.png")} />
-              ),
-              title: "Welcome to TeamUp!",
-              subtitle: "Your ultimate team management app.",
-            },
-          ]}
-        />
-      </SafeAreaView>
+      <SafeAreaView style={styles.container}></SafeAreaView>
     </SafeAreaProvider>
   );
 }
