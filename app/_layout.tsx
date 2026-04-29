8e6f6281c21ee804a5b83598309da3cda2c8c4a4
Marsraad_ <86083007+PanzerschokoladeAddict@users.noreply.github.com>
2026-04-29 16:20:45 +1000

    Home screen and onboarding

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="onboarding" options={{ headerShown: true }} />
+      <Stack.Screen name="scan" options={{ headerShown: false }} />
+      <Stack.Screen name="joined" options={{ headerShown: true }} />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,49 @@
-import { Link } from "expo-router";
+import { useRouter } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View style={styles.container}>
-      <Text>Welcome to the Home Page!</Text>
-      <Link href="/onboarding">Go to Onboarding Page</Link>
-      <TextInput placeholder="Input Holder" />
-    </View>
+    <SafeAreaProvider style={styles.header}>
+      <SafeAreaView style={styles.container}>
+        <Button
+          style={styles.button}
+          mode="contained"
+          buttonColor="#4c8f3f"
+          onPress={() => router.push("/onboarding")}
+        >
+          Create A New Team
+        </Button>
+        <Button
+          style={styles.button}
+          mode="contained"
+          buttonColor="#4c8f3f"
+          onPress={() => router.push("/scan")}
+        >
+          Join An Existing Team
+        </Button>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    padding: 40,
+  },
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    gap: 80,
+  },
+  button: {
+    justifyContent: "center",
+    width: "70%",
+    height: 80,
   },
 });
