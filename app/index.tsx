751ed22cc6ca9c71b2fa2f88f33ce2698ced2bad
Marsraad_ <86083007+PanzerschokoladeAddict@users.noreply.github.com>
2026-04-29 13:57:56 +1000

    Basic start screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Welcome to the Home Page!</Text>
+      <Link href="/onboarding">Go to Onboarding Page</Link>
+      <TextInput placeholder="Input Holder" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
